0dfe1c9af3e99e51884d7bca081bbb7a79f171a5
antony13 <nanosa@uni.coventry.ac.uk>
2017-12-06 21:47:39 +0000

    Use a template HTML file

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"html/template" //add the template library
 )
 
 func main() {
+	templates := template.Must(template.ParseFiles("templates/index.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if err := templates.ExecuteTemplate(w, "index.html", nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 		fmt.Fprint(w, "Hello world!!!!")
 	})
 
