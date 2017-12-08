da10d6b7e18538d3aae0c3a61b680fc3de34508d
antony13 <nanosa@uni.coventry.ac.uk>
2017-12-08 09:39:52 +0000

    Formatted code using go fmt

--- tutorial/calc.go
+++ tutorial/calc.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"calc" //new lib name
+	"fmt"
 )
 
 func main() {
-	var x,y int = 10,3 
-	fmt.Println(calc.Add(x,y))
-	fmt.Println(calc.Subtract(x,y))
+	var x, y int = 10, 3
+	fmt.Println(calc.Add(x, y))
+	fmt.Println(calc.Subtract(x, y))
 }
