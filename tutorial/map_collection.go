06439098dd74e4f1197420a4b26637b6b73b710a
antony13 <nanosa@uni.coventry.ac.uk>
2017-12-08 17:17:16 +0000

    More serious map data structure

--- tutorial/map_collection.go
+++ tutorial/map_collection.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 )
 
+type CityLoc struct {
+	lat, long float64
+}
+
 func main() {
-	dict := make(map[string]string) //similar dictionary
-	//dict := map[string]string
+	dict := make(map[string]string) //similar to python dictionary
 	dict["go"] = "Golang"
 	dict["C#"] = "CSharp"
 	dict["php"] = "PHP"
@@ -14,4 +17,11 @@ func main() {
 	for v := range dict {
 		fmt.Printf("Key: The value is: %s\n", v)
 	}
+	
+	//create a map data structure with a string index and a CityLoc value
+	real_map := make(map[string]CityLoc)
+	real_map["athens"] = CityLoc {
+		37.9838, 23.7275,
+	}
+
 } //end of main
